14328048d86658cf0528b023a9df04923832ec87
GURUPRASAD74167 <guruabhiram79@gmail.com>
2024-11-08 22:14:05 +0530

    bank program logic

---
+++ practise.js
@@ -0,0 +1,159 @@
+
+
+// const x = ['one','two','three',1,2,3,'hallo world'];
+
+
+
+// console.log(x.sort())
+
+
+
+//  x.pop() /*last remove*/
+// x.push('last add');
+// x.shift() /*frist remove */
+// x.unshift('javaScript');
+
+
+
+
+
+// const sum = 'HUNUMAN';
+
+
+// console.log(sum.split('').reverse().join())
+
+
+
+// const x = new Date()
+
+// console.log(x.getDate())
+
+
+// const Nwe_set = new Set()
+//    Nwe_set.add('hallo');
+//    Nwe_set.add('hallo');
+//    Nwe_set.add('guru')
+
+
+   
+//   Nwe_set.clear()
+//    Nwe_set.forEach(i =>{
+//       console.log(i)
+//    })
+
+
+// const data = new Map([
+//    ['one:',32],
+//    ['two:',43],
+//    ['two:',43]
+
+
+// ])
+
+// console.log(data)
+
+
+// const person = {
+//    name:'gurumass',
+//    email:'xyz@gmail.com',
+//    address:'hyderabad',
+
+//    person:function() {
+//       this.person
+      
+//    }
+// };
+
+// person.address =' Bangloore'
+// console.log(person.address)
+
+// for(let x = 0; x < 2;x++){
+//    console.log(x)
+// }
+
+
+
+
+
+
+// var createCounter =function(n){
+//    let counter = n;
+//    return function(){
+//       return counter++
+//    }
+// }
+
+// const counter = createCounter(12)
+
+// console.log(counter() )
+// console.log(counter() )
+// console.log(counter() )
+// console.log(counter() )
+
+
+// const arrow = () => 
+// {
+//    console.log('arow function')
+// }
+
+// arrow()
+
+
+// const x = function(){
+//    console.log('hallo wolrd!')
+// }
+// x()
+
+
+// function Hallo(p1,p2){
+//    return p1 + p2
+
+// }
+
+// Hallo()
+// console.log(3 + 3)
+
+
+
+// const arr = [2002,2003,2004,2005];
+
+// arr.map((i) =>{
+//    console.log(i - 3)
+// })
+
+
+
+// function genretOTP(){
+//    let otp = '';
+
+//    otp = Math.floor(Math.random() * 9000 + 1000);
+
+//    return otp;
+// }
+// console.log(genretOTP())
+
+
+
+// async function name(){
+//    return 123
+// }
+//    console.log(name())
+
+// if(NaN){
+//    console.log('success');
+// }else{
+//    console.log('fail')
+// }
+
+// console.log(NaN === NaN)
+
+// console.log(NaN === 0)
+
+// console.log(NaN === false)
+
+// console.log(NaN === " ")
+
+// console.log(Object.is(NaN,NaN))
+
+
+
